95528a2e0ceb50501fce3683c7741c8711f342b8
Johnathan Van Why <jrvanwhy@gmail.com>
2015-04-16 19:55:45 -0700

    Fixing the indentation of Encoder.m

--- atrias-robot/Encoder.m
+++ atrias-robot/Encoder.m
@@ -10,21 +10,21 @@ classdef Encoder < handle
 		function this = Encoder(rads_per_tick, bits)
 			this.rads_per_tick = rads_per_tick;
             
-            if nargin >= 2
-                this.bits = int64(bits);
-            else
-                this.bits = int64(0);
-            end
-        end % Encoder
+			if nargin >= 2
+				this.bits = int64(bits);
+			else
+				this.bits = int64(0);
+			end
+		end % Encoder
 
 		% This sets up the initial calibration for the encoder.  
 		%
 		% Parameters:
 		%     calib_pos   The current position [rad]
-        %     calib_tics  The current position [tics]
+		%     calib_tics  The current position [tics]
 		function initialize(this, calib_pos, calib_tics)
 			this.calib_pos   = calib_pos;
-            this.calib_tics  = int64(calib_tics);
+			this.calib_tics  = int64(calib_tics);
 			this.initialized = true;
 		end
 
@@ -39,25 +39,25 @@ classdef Encoder < handle
 		%
 		% Returns: The current position (radians) and velocity (rad/s)
 		function [pos, vel] = update(this, new_ticks, new_timestamp)
-            % Offset the tics by the calibration position
-            new_ticks = int64(new_ticks) - this.calib_tics;
+			% Offset the tics by the calibration position
+			new_ticks = int64(new_ticks) - this.calib_tics;
 
 			% Make sure to output zeros until initialize() has been called.
 			if ~this.initialized
 				pos = 0.0;
 				vel = 0.0;
 				return
-            end
+			end
             
-            if nargin < 3
-                new_timestamp = 0;
-            end
+			if nargin < 3
+				new_timestamp = 0;
+			end
 
 			% Update the current position.
 			dticks = new_ticks - this.cur_ticks; % Grab the difference -- this is correct mod 2^bits, but takes on the (incorrect) range (-2^bits, 2^bits)
-            if this.bits ~= 0 % If we need to wrap
-                dticks = mod(dticks + 2^(this.bits-1), 2^this.bits) - 2^(this.bits-1); % This leaves a correct value mod 2^bits, plus now it range (-2^(bits-1), 2^(bits-1))
-            end
+			if this.bits ~= 0 % If we need to wrap
+				dticks = mod(dticks + 2^(this.bits-1), 2^this.bits) - 2^(this.bits-1); % This leaves a correct value mod 2^bits in the range (-2^(bits-1), 2^(bits-1))
+			end
 			this.cur_ticks = this.cur_ticks + dticks;
 
 			% Compute the position (if requested by the caller)
@@ -69,14 +69,14 @@ classdef Encoder < handle
 			if nargout >= 2
 				% Account for the difference in time to sample the sensor
 				dtimestamps = new_timestamp - this.timestamp;
-                % Time between this sample and the previous one
+				% Time between this sample and the previous one
 				dt = this.sample_time + dtimestamps/this.MEDULLA_TIMER_FREQ;
 
 				% Compute the velocity through finite differencing
 				vel = this.rads_per_tick * double(dticks) / double(dt);
-            end
+			end
             
-            % Update the stored position and timestamp data
+			% Update the stored position and timestamp data
 			this.prev_ticks = new_ticks;
 			this.timestamp  = new_timestamp;
 		end
@@ -89,7 +89,7 @@ classdef Encoder < handle
 
 		% Calibration position
 		calib_pos        = 0; % [rad]
-        calib_tics@int64 = int64(0); % [tics]
+		calib_tics@int64 = int64(0); % [tics]
 
 		% Saved state (used for differentiation in update())
 		prev_ticks@int64
@@ -101,10 +101,10 @@ classdef Encoder < handle
 		% Initialization state
 		initialized = false % Whether or not initialize() has been called
 		running     = false % Whether or not ticks and timestamp have been properly initialized -- used to delay velocity updating until 2 samples have been done
-    end
+	end
     
-    properties (Access = private, Constant = true)
-        sample_time = 0.001;
-        MEDULLA_TIMER_FREQ = 32e6;
-    end
+	properties (Access = private, Constant = true)
+		sample_time = 0.001;
+		MEDULLA_TIMER_FREQ = 32e6;
+	end
 end % classdef
