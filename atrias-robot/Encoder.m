222201c8839bfc0a86856ca4867868a6344e4345
Johnathan Van Why <jrvanwhy@gmail.com>
2015-04-19 00:53:12 -0700

    More progress on the Encoder class.

--- atrias-robot/Encoder.m
+++ atrias-robot/Encoder.m
@@ -10,11 +10,8 @@
 
 classdef Encoder < matlab.System
 	properties
-		% Upper raw value (0 for no unwrap)
-		maxRawCnt@int64 = 0
-
-		% Lower raw value (0 for no unwrap)
-		minRawCnt@int64 = 0
+		% Unwrapping modulus (0 for no unwrap)
+		unwrapMod@int64 = 0
 
 		% Initial position output
 		initPosOut@double = 0
@@ -26,6 +23,9 @@ classdef Encoder < matlab.System
 	properties (Logical = true)
 		% Check for non-finite pos or vel
 		checkInf@logical = false
+
+		% Calibrate only once
+		calibOnce@logical = true
 	end
 
 	properties (Access = protected)
@@ -39,8 +39,17 @@ classdef Encoder < matlab.System
 		% Current position relative to the calibration position, in ticks
 		posTicks@int64 = 0
 
+		% Last iteration's tick count, for differentiation.
+		prevTicks@int64 = 0
+
+		% Calibration location
+		calibVal = 0
+
 		% Accumulated delta time (toward the next velocity calculation)
 		dt = 0
+
+		% Output position units per tick (for linear encoders)
+		unitsPerTick@double = 0
 
 		% Data filter properties. These are set at each iteration by
 		% stepImpl. These are properties, rather than passed in,
@@ -85,29 +94,120 @@ classdef Encoder < matlab.System
 			trust = true;
 		end
 
+		% Unwraps a delta value -- decodes a possibly wrapped tick count difference
+		% into the range [-unwrapMod/2, unwrapMod/2)
+		function out = unwrapTicks(this, ticks)
+			% Don't unwrap if unwrapping is disabled
+			if this.unwrapMod <= 0
+				out = ticks;
+				return
+			end
+
+			% The unwrapped tick count must be in the above range and equivalent
+			% to the input modulo this.unwrapMod.
+			% These two restrictions give a unique solution. This line guarantees
+			% that these restrictions hold, and therefore gives the desired solution
+			out = mod(ticks + (this.unwrapMod/2), this.unwrapMod) - (this.unwrapMod/2);
+		end
+
+		% Processes the given position. This takes in a tick count relative to the calibration
+		% location and returns a position. May be overridden for nonlinear encoders
+		function pos = decodePos(this, ticks, calibVal)
+			pos = calibVal + this.unitsPerTick * ticks;
+		end
+
+		% Checks if we should calibrate on this cycle.
+		% This will only be called when calibTrig is true.
+		function out = shouldCalib(this, ticks, calibTicks, calibVal)
+			% Make sure we're not recalibrating unless configured to do so
+			if this.calibOnce && this.calibrated
+				out = false;
+				return
+			end
+
+			% Compute the position given the current tick count and calibration location.
+			% Assumes the encoder is within half a (modular) rotation of the calibration rotation.
+			pos = this.decodePos(this.unwrapTicks(ticks - calibTicks), calibVal);
+
+			% Calibrate iff the data is trustworthy.
+			% We ignore the velocity because we don't have enough information
+			% to compute it yet.
+			out = this.trustData(pos, 0);
+		end
+
+		% Calibration function. Check if we need to calibrate,
+		% calibrate, then record that we've calibrated.
+		function calibrate(this, ticks, calibTicks, calibVal)
+			% Quit of we aren't calibrating this cycle
+			if ~this.shouldCalib(ticks, calibTicks, calibVal)
+				return
+			end
+
+			% Compute the position relative to the calibration location
+			this.posTicks = this.unwrapTicks(ticks - calibTicks);
+
+			% Copy over other calibration values
+			this.calibVal = calibVal;
+
+			% Record that calibration has occurred
+			this.calibrated = true;
+		end
+
+		% Main update routine. Handles position and velocity computation
+		function update(this, ticks, dt)
+			% Compute the position delta and new position tick count
+			dticks = this.unwrapTicks(ticks - this.prevTicks);
+			newPosTicks = this.posTicks + dticks;
+
+			% Update the delta time for the velocity calculation
+			this.dt = this.dt + dt;
+
+			% Compute the new position and velocity
+			pos = this.decodePos(newPosTicks, this.calibVal);
+			vel = (pos - this.pos) / this.dt;
+
+			% Check if it's acceptable. If not, quit early (throwing out the data)
+			if ~this.trustData(pos, vel)
+				return
+			end
+
+			% Store the new values, and reset the delta time for the next iteration
+			this.posTicks = newPosTicks;
+			this.pos      = pos;
+			this.vel      = vel;
+			this.dt       = 0;
+		end
+
 		% Simulink's update function. Called at each model iteration.
-		% See the top of this file for parameter documentation
-		function [pos, vel, isValid] = stepImpl(this, ticks, dt, calibTicks, calibVal, calibTrig, minPos, maxPos, minVel, maxVel)
+		% See the top of this file for parameter documentation.
+		% This should be overrode and called by subclasses
+		function [pos, vel, isValid] = stepImpl(this, ticks, dt, calibTicks, calibVal, calibTrig, unitsPerTick, minPos, maxPos, minVel, maxVel)
 			% Copy over the filter settings
 			this.minPos = minPos;
 			this.maxPos = maxPos;
 			this.minVel = minVel;
 			this.maxVel = maxVel;
 
-			% Run the calibration routine, if the calibration trigger has been specified
-			if calibTrig
-				this.calibrate(ticks, calibTicks, calibVal)
-			end
+			% Copy over miscellaneous settings
+			this.unitsPerTick = unitsPerTick;
 
 			% Compute a new position iff calibration's already been done.
 			if this.calibrated
 				this.update(ticks, dt)
 			end
 
+			% Run the calibration routine, if the calibration trigger has been specified
+			if calibTrig
+				this.calibrate(ticks, calibTicks, calibVal)
+			end
+
 			% Set our outputs
 			pos     = this.pos;
 			vel     = this.vel;
 			isValid = this.calibrated;
+
+			% Copy over the previous values for differentiation purposes
+			this.prevTicks = ticks;
 		end
 	end
 end
